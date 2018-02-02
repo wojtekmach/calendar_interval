e3d962447aadde5f2f9b47159e5212d301e1ee7f
Wojtek Mach <wojtek@wojtekmach.pl>
2018-02-02 21:57:36 +0100

    Add basic parse!/1

--- lib/calendar_interval.ex
+++ lib/calendar_interval.ex
@@ -1,2 +1,54 @@
 defmodule CalendarInterval do
+  defstruct [:first, :last, :precision]
+
+  @type t() :: %CalendarInterval{
+          first: NaiveDateTime.t(),
+          last: NaiveDateTime.t(),
+          precision: precision()
+        }
+
+  @type precision() :: :year | :month | :day | :hour | :minute | :second
+
+  @patterns [
+    {:year, 4, "-01-01 00:00:00"},
+    {:month, 7, "-01 00:00:00"},
+    {:day, 10, " 00:00:00"},
+    {:hour, 13, ":00:00"},
+    {:minute, 16, ":00"}
+  ]
+
+  @doc """
+  Parses a string into an interval.
+
+  ## Examples
+
+      iex> CalendarInterval.parse!("2018-06-30")
+      %CalendarInterval{first: ~N[2018-06-30 00:00:00], last: ~N[2018-06-30 23:59:59], precision: :day}
+
+  """
+  @spec parse!(String.t()) :: t()
+  def parse!(string) do
+    {ndt, precision} = do_parse!(string)
+    last = next(ndt, precision) |> prev(:second)
+    %CalendarInterval{first: ndt, last: last, precision: precision}
+  end
+
+  for {precision, bytes, rest} <- @patterns do
+    defp do_parse!(<<_::unquote(bytes)-bytes>> = string) do
+      do_parse!(string <> unquote(rest))
+      |> put_elem(1, unquote(precision))
+    end
+  end
+
+  defp do_parse!(string) do
+    ndt = NaiveDateTime.from_iso8601!(string)
+    {ndt, :second}
+  end
+
+  defp next(ndt, :year), do: update_in(ndt.year, & &1 + 1)
+  defp next(ndt, :day), do: NaiveDateTime.add(ndt, 24 * 60 * 60, :second)
+
+  defp prev(ndt, :second) do
+    NaiveDateTime.add(ndt, -1, :second)
+  end
 end
--- test/calendar_interval_test.exs
+++ test/calendar_interval_test.exs
@@ -1,4 +1,16 @@
 defmodule CalendarIntervalTest do
   use ExUnit.Case
   doctest CalendarInterval
+
+  test "parse!/1" do
+    i = CalendarInterval.parse!("2018")
+    assert i.precision == :year
+    assert i.first == ~N"2018-01-01 00:00:00"
+    assert i.last == ~N"2018-12-31 23:59:59"
+
+    i = CalendarInterval.parse!("2018-06-15")
+    assert i.precision == :day
+    assert i.first == ~N"2018-06-15 00:00:00"
+    assert i.last == ~N"2018-06-15 23:59:59"
+  end
 end
