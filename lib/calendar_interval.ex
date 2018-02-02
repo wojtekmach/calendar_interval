08a435997d67fb32590b7a2bf01b9aad00458131
Wojtek Mach <wojtek@wojtekmach.pl>
2018-02-02 22:05:52 +0100

    Handle microseconds

--- lib/calendar_interval.ex
+++ lib/calendar_interval.ex
@@ -7,14 +7,20 @@ defmodule CalendarInterval do
           precision: precision()
         }
 
-  @type precision() :: :year | :month | :day | :hour | :minute | :second
+  @type precision() :: :year | :month | :day | :hour | :minute | :second | {:microsecond, 1..6}
 
   @patterns [
-    {:year, 4, "-01-01 00:00:00"},
-    {:month, 7, "-01 00:00:00"},
-    {:day, 10, " 00:00:00"},
-    {:hour, 13, ":00:00"},
-    {:minute, 16, ":00"}
+    {:year, 4, "-01-01 00:00:00.000000"},
+    {:month, 7, "-01 00:00:00.000000"},
+    {:day, 10, " 00:00:00.000000"},
+    {:hour, 13, ":00:00.000000"},
+    {:minute, 16, ":00.000000"},
+    {:second, 19, ".000000"},
+    {{:microsecond, 1}, 21, "00000"},
+    {{:microsecond, 2}, 22, "0000"},
+    {{:microsecond, 3}, 23, "000"},
+    {{:microsecond, 4}, 24, "00"},
+    {{:microsecond, 5}, 25, "0"}
   ]
 
   @doc """
@@ -23,13 +29,13 @@ defmodule CalendarInterval do
   ## Examples
 
       iex> CalendarInterval.parse!("2018-06-30")
-      %CalendarInterval{first: ~N[2018-06-30 00:00:00], last: ~N[2018-06-30 23:59:59], precision: :day}
+      %CalendarInterval{first: ~N[2018-06-30 00:00:00.000000], last: ~N[2018-06-30 23:59:59.999999], precision: :day}
 
   """
   @spec parse!(String.t()) :: t()
   def parse!(string) do
     {ndt, precision} = do_parse!(string)
-    last = next(ndt, precision) |> prev(:second)
+    last = next(ndt, precision) |> prev({:microsecond, 6})
     %CalendarInterval{first: ndt, last: last, precision: precision}
   end
 
@@ -40,15 +46,28 @@ defmodule CalendarInterval do
     end
   end
 
-  defp do_parse!(string) do
-    ndt = NaiveDateTime.from_iso8601!(string)
-    {ndt, :second}
+  defp do_parse!(<<_::26-bytes>> = string) do
+    {NaiveDateTime.from_iso8601!(string), {:microsecond, 6}}
   end
 
-  defp next(ndt, :year), do: update_in(ndt.year, & &1 + 1)
-  defp next(ndt, :day), do: NaiveDateTime.add(ndt, 24 * 60 * 60, :second)
+  defp next(ndt, :year), do: update_in(ndt.year, &(&1 + 1))
 
-  defp prev(ndt, :second) do
-    NaiveDateTime.add(ndt, -1, :second)
+  defp next(ndt, precision) do
+    {count, unit} = precision_to_count_unit(precision)
+    NaiveDateTime.add(ndt, count, unit)
+  end
+
+  defp prev(ndt, precision) do
+    {count, unit} = precision_to_count_unit(precision)
+    NaiveDateTime.add(ndt, -count, unit)
+  end
+
+  defp precision_to_count_unit(:day), do: {24 * 60 * 60, :second}
+  defp precision_to_count_unit(:hour), do: {60 * 60, :second}
+  defp precision_to_count_unit(:minute), do: {60, :second}
+  defp precision_to_count_unit(:second), do: {1, :second}
+
+  defp precision_to_count_unit({:microsecond, exponent}) do
+    {1, Enum.reduce(1..exponent, 1, fn _, acc -> acc * 10 end)}
   end
 end
--- test/calendar_interval_test.exs
+++ test/calendar_interval_test.exs
@@ -5,12 +5,22 @@ defmodule CalendarIntervalTest do
   test "parse!/1" do
     i = CalendarInterval.parse!("2018")
     assert i.precision == :year
-    assert i.first == ~N"2018-01-01 00:00:00"
-    assert i.last == ~N"2018-12-31 23:59:59"
+    assert i.first == ~N"2018-01-01 00:00:00.000000"
+    assert i.last == ~N"2018-12-31 23:59:59.999999"
 
     i = CalendarInterval.parse!("2018-06-15")
     assert i.precision == :day
-    assert i.first == ~N"2018-06-15 00:00:00"
-    assert i.last == ~N"2018-06-15 23:59:59"
+    assert i.first == ~N"2018-06-15 00:00:00.000000"
+    assert i.last == ~N"2018-06-15 23:59:59.999999"
+
+    i = CalendarInterval.parse!("2018-06-15 10:20:30.123")
+    assert i.precision == {:microsecond, 3}
+    assert i.first == ~N"2018-06-15 10:20:30.123000"
+    assert i.last == ~N"2018-06-15 10:20:30.123999"
+
+    i = CalendarInterval.parse!("2018-06-15 10:20:30.123456")
+    assert i.precision == {:microsecond, 6}
+    assert i.first == ~N"2018-06-15 10:20:30.123456"
+    assert i.last == ~N"2018-06-15 10:20:30.123456"
   end
 end
