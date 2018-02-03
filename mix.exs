cb7a6e06bd0b83ce4da7802870994396fef6ddb2
Wojtek Mach <wojtek@wojtekmach.pl>
2018-02-03 10:04:57 +0100

    Set up ex_doc

--- mix.exs
+++ mix.exs
@@ -1,12 +1,15 @@
 defmodule CalendarInterval.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :calendar_interval,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
+      docs: docs(),
       deps: deps()
     ]
   end
@@ -17,7 +20,17 @@ defmodule CalendarInterval.MixProject do
     ]
   end
 
+  defp docs() do
+    [
+      main: "CalendarInterval",
+      source_url: "https://github.com/wojtekmach/calendar_interval",
+      source_ref: "v#{@version}"
+    ]
+  end
+
   defp deps do
-    []
+    [
+      {:ex_doc, github: "elixir-lang/ex_doc", only: :dev}
+    ]
   end
 end
