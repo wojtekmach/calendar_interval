cab219d18a05f93078699be8f82f21f3be7b450a
Wojtek Mach <wojtekmach@users.noreply.github.com>
2019-09-15 14:08:09 +0200

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule CalendarInterval.MixProject do
   use Mix.Project
 
-  @version "0.2.0"
+  @version "0.2.0-dev"
   @source_url "https://github.com/wojtekmach/calendar_interval"
 
   def project() do
